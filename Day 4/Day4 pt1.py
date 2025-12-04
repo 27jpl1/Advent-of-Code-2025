8c15b26b12bb0455f2c039819d5dd05876e8a688
27jpl1 <japylyon@gmail.com>
2025-12-04 08:55:23 -0600

    Day 4

---
+++ Day 4/Day4 pt1.py
@@ -0,0 +1,30 @@
+file = open("Rolls", "r").readlines()
+
+rolls = []
+
+for line in file:
+    roll = []
+    for char in line.strip():
+        roll.append(char)
+    rolls.append(roll)
+
+count = 0
+directions = [(0,1),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1),(1,0),(1,1)]
+row = 0
+while row < len(rolls):
+    col = 0
+    while col < len(rolls[0]):
+        if rolls[row][col] == "@":
+            total = 0
+            for direction in directions:
+                new_row = row + direction[0]
+                new_col = col + direction[1]
+                if 0 <= col + direction[1] < len(rolls) and 0 <= row + direction[0] < len(rolls[0]):
+                    if rolls[new_row][new_col] == "@":
+                        total += 1
+            if total < 4:
+                if rolls[row][col] == "@":
+                    count += 1
+        col += 1
+    row += 1
+print(count)
